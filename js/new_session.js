04ff3ebc59d054f6832258dfde2784e0766a3849
Shlomi <vgtvgy1@gmail.com>
2020-12-29 03:58:10 +0200

    Parent window now treated as modal

--- js/new_session.js
+++ js/new_session.js
@@ -2,20 +2,21 @@ const electron = require('electron')
 const BrowserWindow = electron.remote.BrowserWindow
 const path = require('path')
 const url = require('url')
+const remote = require ("electron").remote;
+
+
 
 document.querySelector('#btn_newSession').addEventListener('click', () => {
-	alert("Ok")
-	let win = new BrowserWindow({ show: false })
-	win.on('close', function () { win = null })
-//	win.loadURL("../html/dummy.html")
+	const mainWindow = remote.getCurrentWindow ();
 
-win.loadURL(url.format({
-    pathname: path.join(__dirname, '../html/dummy.html'),
-    protocol: 'file',
-    slashes: true
-  }))
-
-	win.once('ready-to-show', () => {
-  		win.show()
+	let child = new BrowserWindow({parent: mainWindow, modal: true, show: false})
+	child.loadURL(url.format({
+		pathname: path.join(__dirname, '../html/dummy.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+	child.once('ready-to-show', () => {
+		child.show()
 	})
+	child.on('close', function () { child = null })
 })
