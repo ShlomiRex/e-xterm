e520d45e1fb458ebe7b4098b3a5ba3b9acf61783
Shlomi <vgtvgy1@gmail.com>
2020-12-29 02:49:29 +0200

    Fix windows has problems with node-pty, updated npm_install.bat

--- npm_install.bat
+++ npm_install.bat
@@ -1,8 +1,3 @@
 @echo off
-setlocal
-set npm_config_disturl="https://atom.io/download/electron"
-set npm_config_target=9.1.0
-set npm_config_runtime="electron"
-set npm_config_cache=~\.npm-electron
 npm i
-endlocal
+.\node_modules\.bin\electron-rebuild.cmd
