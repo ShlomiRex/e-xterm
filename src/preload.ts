e430de25ffd466b71e1d911b1a0498b6630c4342
Shlomi <vgtvgy1@gmail.com>
2021-02-12 23:16:17 +0200

    Move contextmenu to preload instead of renderer

--- src/preload.ts
+++ src/preload.ts
@@ -2,7 +2,7 @@
 // It has the same sandbox as a Chrome extension.
 
 import * as Store from 'electron-store';
-import { ipcRenderer } from 'electron';
+import { BrowserWindow, ipcRenderer, remote} from 'electron';
 
 import { SSHSession } from './session';
 import { Tabs, Tab } from "./tabs";
@@ -22,12 +22,12 @@ window.addEventListener("DOMContentLoaded", () => {
 		console.log("Session:", session)
 		console.log("Password length:", password.length)
 
-		if(session.username != null && session.username.length > 0) {
+		if (session.username != null && session.username.length > 0) {
 			username = session.username
 		}
 
 		let title = username;
-		if(! title) {
+		if (!title) {
 			title = username + "@" + session.remote_host
 		}
 
@@ -64,7 +64,7 @@ window.addEventListener("DOMContentLoaded", () => {
 			console.log("Close emitted, hadError? ", hadError);
 			Tabs.getInstance().removeTab(tab.id);
 		});
-		
+
 
 		let hostname = session.remote_host;
 		let port = session.port;
@@ -105,3 +105,57 @@ ipcRenderer.on("WindowResize", (ev, size: Array<number>) => {
 	console.log("Preload - fit terminal");
 	tabs.fit_terminal()
 });
+
+const { Menu, MenuItem } = remote;
+
+let settings_clicked = (menuItem: any) => {
+	console.log("Clicked on settings", menuItem.id)
+	
+}
+let deleteBookmark_called = (menuItem: any) => {
+	console.log("Clicked on delete bookmark", menuItem.id)
+};
+
+const bookmarkContextMenu = new Menu();
+bookmarkContextMenu.append(new MenuItem({
+	"click": settings_clicked,
+	"label": 'Settings',
+	"id": "settings"
+}));
+bookmarkContextMenu.append(new MenuItem({
+	"type": "separator"
+}));
+bookmarkContextMenu.append(new MenuItem({
+	"click": deleteBookmark_called,
+	"label": "Delete bookmark",
+	"id": "delete"
+}));
+
+
+
+window.addEventListener('contextmenu', (e: MouseEvent) => {
+	e.preventDefault()
+	console.log("Context menu fired!", e)
+
+	//Find if path is bookmark. Object may not have "hasAttribute"
+	try {
+		for (let path of (e as any).path) {
+			if (path.hasAttribute("data-bookmark-id")) {
+				let bookmarkId = path.getAttribute("data-bookmark-id");
+				console.log("Right clicked on bookmark id:", bookmarkId)
+				let callback = () => {
+					console.log("ContextMenu closed")
+				};
+				bookmarkContextMenu.popup({
+					"window": remote.getCurrentWindow(),
+					"callback": callback
+				})
+				break
+			}
+		}
+	} catch (e: any) {
+		//do nothing
+	}
+
+
+}, false)
--- src/renderer.ts
+++ src/renderer.ts
@@ -211,14 +211,3 @@ ipcRenderer.on("Renderer_BookmarksUI_UpdateBookmark", (ev, session: SSHSession) 
 	BookmarksUI.getInstance().update(bookmarkId, text);
 });
 
-const { Menu, MenuItem } = remote
-const menu = new Menu()
-menu.append(new MenuItem({ label: 'MenuItem1', click() { console.log('item 1 clicked') } }))
-menu.append(new MenuItem({ type: 'separator' }))
-menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }))
-
-window.addEventListener('contextmenu', (e) => {
-	e.preventDefault()
-	console.log("Context menu fired!", e)
-	menu.popup({ window: remote.getCurrentWindow() })
-}, false)
