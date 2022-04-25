a2e2f475e51d437f0d9e0073960882aeff497525
Shlomi <vgtvgy1@gmail.com>
2022-04-26 00:34:43 +0300

    add todo

--- src/shell_terminal.ts
+++ src/shell_terminal.ts
@@ -2,10 +2,10 @@ import * as pty from 'node-pty';
 import * as os from 'os';
 
 var isRenderer = (process && process.type === 'renderer')
-console.log	('isRenderer', isRenderer)
+console.debug('isRenderer', isRenderer)
 
 import { BasicTerminal, Shell } from './terminal';
-console.log("Platform:", os.platform())
+console.debug("Platform:", os.platform())
 
 const WINDOWS = os.platform() === 'win32';
 const MAC = os.platform() == "darwin";
@@ -24,23 +24,23 @@ export class ShellTerminal extends BasicTerminal {
 		} else {
 			shell = Shell.BASH
 		}
-		console.log("Chosen shell:", shell);
+		console.debug("Chosen shell:", shell);
 
-		// this.ptyProcess = pty.spawn(shell, [], {
-		// 	name: 'xterm-color',
-		// 	cwd: process.cwd(),
-		// 	env: process.env,
-		// 	rows: this.rows,
-		// 	cols: this.cols
-		// });
+		this.ptyProcess = pty.spawn(shell, [], {
+			name: 'xterm-color',
+			cwd: process.cwd(),
+			env: process.env,
+			rows: this.rows,
+			cols: this.cols
+		});
 
-		// this.ptyProcess.onData((data: any) => {
-		// 	this.write(data);
-		// });
+		this.ptyProcess.onData((data: any) => {
+			this.write(data);
+		});
 
-		// //When user types(input), write to node-pty
-		// this.onData((data: any) => {
-		// 	this.ptyProcess.write(data)
-		// });
+		//When user types(input), write to node-pty
+		this.onData((data: any) => {
+			this.ptyProcess.write(data)
+		});
 	}
 }
